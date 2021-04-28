9d849b31610e30cfedd002fc86b13f1c1dd0b2fd
brokensythe <ade_1.0@live.co.uk>
2021-04-28 09:06:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+alert(`Welcome to the password generator`)
+const userFirstName = prompt(`What is your first name?`)
+confirm(`Can you confirm that your first name is ${userFirstName}?`)
+const userLastName = prompt(`What is your last name?`)
+confirm(`Can you confirm that your last name is ${userLastName}?`)
+let userColor = prompt(`What is your favourite colour?`)
+confirm(`Can you confirm that you favourite colour is ${userColor}?`)
+let randomNumber = String(Math.round(Math.random() * 200))
+alert(`Your extremely insecure password is ${userFirstName}${userLastName}${userColor}${randomNumber}`)
+
+
+
+console.log("Your password is", userFirstName,userLastName,userColor,randomNumber)
